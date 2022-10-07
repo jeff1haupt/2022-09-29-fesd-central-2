9999ee4f5df2646912d0c35b64ec54477842412b
jeff1haupt <jeff.lawjmh@gmail.com>
2022-10-06 21:35:57 -0400

    first commit of classroom examples

---
+++ Week-1/app.js
@@ -0,0 +1,44 @@
+// <-- This is a single comment
+
+/*
+    This is a multiline comment
+*/
+
+// declaring variables and setting their values for the first time
+let number1 = 5; // number types 
+const number2 = 3.14;
+var number3 = 15; // let, const, var - variable name - = the value
+let number4;
+let number5, number6, number7; // this is okay, but you should avoid
+number6 = 2;
+
+const numberEight = 10;
+
+
+let name1 = "Hannah"; // String types
+const name2 = 'Natalie';
+var name3 = `Lars`;
+
+// variable naming conventions should look like these variable names
+let isItFall = true; // Boolean types 
+const isFailing = false; 
+var isVarStillUsed = true;
+
+//To show things in the console, we use:
+console.log("Hello class - from the console!");
+
+// Mathmetical calculations and operations
+// PEMDAS - Parenthesis, exponents, multiplication & division, addition & subtriction
+console.log(100 - 99); // print out the number - 1 to the console.
+console.log(number1 + number2); // prints out 8.14 
+console.log(number1 * number6); // 5 x 2 = 10;
+console.log(number1 / 2); // 2.5
+console.log(8 % 3); // 0
+console.log(111 % 32); // gives us the remainder after dividing 111 by 32 
+
+// Boolean operations 
+console.log( 10 < 100 ); // true
+var isNum1Larger = number1 > 100 // 5 > 2;
+console.log(isNum1Larger);
+
+
